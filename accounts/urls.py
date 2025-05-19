4e6ac9727205e3573015d91ebfb7155c7bc45abf
Gabriel <gabriel_gpu@outlook.com>
2025-05-19 20:50:10 -0300

    feat(accounts): implementar views de login, logout e cadastro com templates básicos

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,5 @@ from .views import UserLoginView, UserLogoutView, UserCreateView
 urlpatterns = [
   path('login/', UserLoginView.as_view(), name='login'),
   path('logout/', UserLogoutView.as_view(), name='logout'),
-  path('register', UserCreateView.as_view(), name='register')
+  path('register/', UserCreateView.as_view(), name='register')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,27 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib import messages
+from django.urls import reverse_lazy
+
+from .forms import SignUpForm
+
+
+class UserLoginView(LoginView):
+  template_name = 'login.html'
+  redirect_authenticated_user = True
+  success_message = 'Login realizado com sucesso!'
+  error_message = 'Usuário ou senha incorretos.'
+
+class UserLogoutView(LogoutView):
+  template_name = 'logout.html'
+  success_message = 'Logout realizado com sucesso!'
+
+  def dispatch(self, request, *args, **kwargs):
+    messages.success(self.request, self.success_message)
+    return super().dispatch(request, *args, **kwargs)
+
+class UserCreateView(CreateView):
+  form_class = SignUpForm
+  template_name = 'register.html'
+  success_url = reverse_lazy('products')
+  success_message = 'Cadastro realizado com sucesso!'
