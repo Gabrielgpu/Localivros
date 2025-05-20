16a3f68ef6fa20b5970a9dfa76ead7d7a4c24c27
Gabriel <gabriel_gpu@outlook.com>
2025-05-20 12:43:07 -0300

    feat(books): criar modelo Book e relacionamento com usuário

--- books/models.py
+++ books/models.py
@@ -1,3 +1,31 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='livros'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True)
+    price = models.CharField(max_length=255, blank=True)
+    gtin_ean = models.CharField(max_length=255, blank=True)
+    width = models.CharField(max_length=100, blank=True)
+    height = models.CharField(max_length=100, blank=True)
+    depth = models.CharField(max_length=100, blank=True)
+    mark = models.CharField(max_length=255, blank=True)
+    short_description = models.TextField(blank=True)
+    url_external_images = models.TextField(blank=True)
+    product_category = models.CharField(max_length=255, blank=True)
+    additional_information = models.TextField(blank=True)
+    ano_de_publicacao = models.CharField(max_length=4, blank=True)
+    page_of_number = models.CharField(max_length=10, blank=True)
+    author = models.CharField(max_length=255, blank=True)
+    language = models.CharField(max_length=100, blank=True)
+    isbn10 = models.CharField(max_length=20, blank=True)
+
+    def __str__(self):
+        return self.short_description or self.author or 'Livro'
--- books/views.py
+++ books/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
 
-# Create your views here.
+
+
+class BookCreateView(CreateView):
+  pass
+
+class BookListView(ListView):
+  pass
+
+class BookDetailView(DetailView):
+  pass
+
+class BookUpdateView(UpdateView):
+  pass
+
+class BookDeleteView(DeleteView):
+  pass
+
+
+
