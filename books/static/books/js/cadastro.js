00aa09bfb1b28c9ddce880164e9a4dd909426c1e
Gabriel <gabriel_gpu@outlook.com>
2025-06-02 19:31:17 -0300

    feat(books): adiciona funcionalidades ao script.js

--- books/static/books/js/cadastro.js
+++ books/static/books/js/cadastro.js
@@ -1,17 +1,31 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const dropdownItems = document.querySelectorAll('.dropdown-item');
-  const selectedUnit = document.getElementById('selected-unit');
-  const submitButton = document.getElementById('submit-button');
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("dropdown-button");
+  const menu = document.getElementById("dropdown-menu");
+  const selected = document.getElementById("selected-unit");
   const stockInput = document.getElementById('stock-input');
 
+  stockInput.value = selected.textContent
 
-  dropdownItems.forEach(item => {
-    item.addEventListener('click', function (event) {
-      event.preventDefault();
-      const value = this.getAttribute('data-value');
-      selectedUnit.textContent = value + ' ' + (value === '1' ? 'Unidade' : 'Unidades');
-      stockInput.value = value;
-      submitButton.style.display = 'inline-block';
+  button.addEventListener("click", (e) => {
+    e.stopPropagation();
+    menu.classList.toggle("hidden");
+  });
+
+
+  menu.querySelectorAll("a[data-value]").forEach(item => {
+    item.addEventListener("click", function (e) {
+      e.preventDefault();
+      const value = this.getAttribute("data-value");
+      selected.textContent = value;
+      stockInput.value = value
+      menu.classList.add("hidden");
     });
+  });
+
+
+  document.addEventListener("click", function (e) {
+    if (!button.contains(e.target) && !menu.contains(e.target)) {
+      menu.classList.add("hidden");
+    }
   });
 });
